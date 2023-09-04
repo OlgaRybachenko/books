620baa3a9913034374f48413188f3502b2182654
OlgaRybachenko <rybachenkoolga@yandex.by>
2023-09-04 08:36:01 +0300

    key

--- src/api/booksApi.js
+++ src/api/booksApi.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const BASE_URL = 'https://www.googleapis.com/books/v1/';
 export function getBookBySearchTerm(searchTerm) {
-  return axios.get(`${BASE_URL}volummes?q=${searchTerm}`);    //получить книги ро поиску
+  return axios.get(`${BASE_URL}volumes?q=${searchTerm}&key=AIzaSyAt-3weR-sdaGb4JsdP3ktm08QMy6TUtx8`);    //получить книги по поиску
 }
 
 export function getBookById(bookId) {
-  return axios.get(`${BASE_URL}volummes/${bookId}`);   //получить книги по id
+  return axios.get(`${BASE_URL}volumes/${bookId}&key=AIzaSyAt-3weR-sdaGb4JsdP3ktm08QMy6TUtx8`);   //получить книги по id
 }
